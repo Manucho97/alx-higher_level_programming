a7ecfc7b560000d331272f283508970a27f69633
manucho97 <emmanuelmukaisimasai@gmail.com>
2024-03-06 23:51:01 +0300

    modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+counter = 0
+n = len(sys.argv)
+if n == 1:
+    print("{} arguments.".format(n - 1))
+elif n == 2:
+    print("{} argument".format(n - 1))
+else:
+    print("{} arguments:".format(n - 1))
+for i in range(1, n):
+    counter += 1
+    print("{}: {}".format(counter, sys.argv[i]))
