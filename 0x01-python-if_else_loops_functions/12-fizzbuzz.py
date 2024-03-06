b8fbb9cece0e8b65316f22790fb8ef68aaf15813
manucho97 <emmanuelmukaisimasai@gmail.com>
2024-03-07 02:11:15 +0300

    executable

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
-x = 1
-while x <= 100:
-    if x % 3 == 0 and x % 5 == 0:
-        print("FizzBuzz ", end="")
-    elif x % 3 == 0:
-        print("Fizz ", end='')
-    elif x % 5 == 0:
-        print("Buzz ", end='')
-    else:
-        print(f"{x} ", end='')
-    x += 1
-print()
+def fizzbuzz():
+    x = 1
+    while x <= 100:
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif x % 3 == 0:
+            print("Fizz ", end='')
+        elif x % 5 == 0:
+            print("Buzz ", end='')
+        else:
+            print(f"{x} ", end='')
+            x += 1
+    print()
